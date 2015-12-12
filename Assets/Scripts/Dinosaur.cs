2264eced0d0dd5e6c1452a4d740512dc39aac7ec
Tyler Manser <tylermanser@gmail.com>
2015-12-12 12:42:44 -0700

    committing dinosaur file

--- Assets/Scripts/Dinosaur.cs
+++ Assets/Scripts/Dinosaur.cs
@@ -2,14 +2,12 @@
 using System.Collections;
 
 public class Dinosaur : MonoBehaviour {
+	
+	public SpriteRenderer ProfilePicture;
+	public string Name;
+	public string Age;
+	public string BiographyOne;
+	public string BiographyTwo;
+	public int Size;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
