116a3dc1f199749ba108912795d932882c7c380c
Chris Schaefbauer <schaefba@colorado.edu>
2015-12-12 14:42:54 -0700

    Make GameManager a singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,28 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager {
 
 	public const int POSITIVE_STATE = 1;
 	public const int NEUTRAL_STATE = 0;
 	public const int NEGATIVE_STATE = -1;
+	public const int INITIAL_DAYS_UNTIL_STARVE = 5;
 
-	// Use this for initialization
-	void Start () {
 
-		LoadOutcomeText (POSITIVE_STATE);
+	public int daysUntilStarve = INITIAL_DAYS_UNTIL_STARVE;
+	public int totalGameDays = 0;
+
+	private int daysGoneBy = 0;
+
+
+	private static GameManager instance;
+
+	// make sure the constructor is private, so it can only be instantiated here
+	private GameManager() {
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
+	public static GameManager Instance {
+		get {
+			if(instance==null) {
+				instance = new GameManager();
+			}
 
+			return instance;
+		}
 	}
 
 	void LoadOutcomeText(int status){
 		string outcomeString = OutcomeStrings.getRandomOutcomeText (status);
 		Text outcomeText = GameObject.Find ("Canvas/SummaryText").GetComponent<Text>();
 		outcomeText.text = outcomeString;
+	}
+
+	public void RestartGame() {
+		SceneManager.LoadScene ("MenuScene", LoadSceneMode.Single);
 	}
 }
