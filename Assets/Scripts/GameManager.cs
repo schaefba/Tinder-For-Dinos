e8dacef97233e7074b38220fdc96e5f5683f42e8
Chris Schaefbauer <schaefba@colorado.edu>
2015-12-12 12:39:40 -0700

    Basics of outcome text setting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+
+	public const int POSITIVE_STATE = 1;
+	public const int NEUTRAL_STATE = 0;
+	public const int NEGATIVE_STATE = -1;
 
 	// Use this for initialization
 	void Start () {
 
-
+		LoadOutcomeText (POSITIVE_STATE);
 	}
 	
 	// Update is called once per frame
@@ -15,8 +20,9 @@ public class GameManager : MonoBehaviour {
 
 	}
 
-	void LoadOutcomeText(){
-
-
+	void LoadOutcomeText(int status){
+		string outcomeString = OutcomeStrings.getRandomOutcomeText (status);
+		Text outcomeText = GameObject.Find ("Canvas/SummaryText").GetComponent<Text>();
+		outcomeText.text = outcomeString;
 	}
 }
--- Assets/Scripts/OutcomeStrings.cs
+++ Assets/Scripts/OutcomeStrings.cs
@@ -1,7 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Strings : MonoBehaviour {
+public class OutcomeStrings : MonoBehaviour {
 
+	public const int POSITIVE_STATE = 1;
+	public const int NEUTRAL_STATE = 0;
+	public const int NEGATIVE_STATE = -1;
+
+
+	protected Dictionary<int, string> statusChanges = new Dictionary<int, string>(){ 
+		{POSITIVE_STATE, "+1 food"}, 
+		{NEUTRAL_STATE, "-1 food"}, 
+		{NEGATIVE_STATE,"Game over."}
+	};
+		
+	protected static List<string> positiveOutcomeChanges = new List<string> { "Om nom nom, that's a tasty meal.", "Positive 2" };
+
+	protected static List<string> neutralOutcomeChanges = new List<string> { "Looks like you couldn't get that one down.", "You were the same size? How surprising" };
+
+	protected static List<string> negativeOutcomeChanges = new List<string> { "Everything looks smaller on the internet.", "Negative 2" };
+
+	public string getStatusChangeText(int status){
+
+		return statusChanges[status];
+	}
+
+	public static string getRandomOutcomeText(int status){
+
+		switch (status) {
+			case POSITIVE_STATE:
+				return positiveOutcomeChanges [Random.Range (0, positiveOutcomeChanges.Count)];
+			case NEUTRAL_STATE:
+				return neutralOutcomeChanges[Random.Range(0,neutralOutcomeChanges.Count)];
+			case NEGATIVE_STATE:
+				return negativeOutcomeChanges [Random.Range (0, negativeOutcomeChanges.Count)];
+			default:
+				throw new UnityException ("Incorrect status.");
+		}
+	}
 
 }
