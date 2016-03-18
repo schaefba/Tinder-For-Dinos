0058189a82ee454486c9b5a06c23ed022560cff7
Chris Schaefbauer <schaefba@colorado.edu>
2016-03-17 22:50:23 -0600

    Add map image and beginning scripts

---
+++ Assets/Scripts/WorldMap.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WorldMap : MonoBehaviour {
+
+
+	public List<Zone> zones;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Zone.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Zone : MonoBehaviour {
+
+	public GameObject[] neighbors;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
