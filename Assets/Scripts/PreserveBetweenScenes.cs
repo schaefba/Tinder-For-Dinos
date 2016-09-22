cf077131f8da670f05597e254fd4a5a791ec3550
Chris Schaefbauer <schaefba@colorado.edu>
2016-09-22 09:13:58 -0700

    Add script to preserve attached object between scenes.

---
+++ Assets/Scripts/PreserveBetweenScenes.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreserveBetweenScenes : MonoBehaviour {
+
+    void Awake()
+    {
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+
+}
