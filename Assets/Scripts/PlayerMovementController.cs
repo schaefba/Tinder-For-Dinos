70ea2cbdffbeab2b813cdd70ec907eec1a43cc59
Chris Schaefbauer <schaefba@colorado.edu>
2016-09-27 22:48:08 -0700

    Apply bounding box to restrict room movement.

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -6,12 +6,35 @@ public class PlayerMovementController : MonoBehaviour {
     [SerializeField]
     private float _playerSpeed;
 
+    private float _minX;
+    private float _maxX;
+    private float _minY;
+    private float _maxY;
+
+    private float _spriteSizeX;
+    private float _spriteSizeY;
+
     // Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    Renderer spriteRenderer = gameObject.GetComponent<Renderer>();
+	    _spriteSizeX = spriteRenderer.bounds.size.x;
+	    _spriteSizeY = spriteRenderer.bounds.size.y;
+
+	    GameObject boundingBox = GameObject.Find("MovementBoundingBox");
+
+	    if (boundingBox != null)
+	    {
+	        Renderer bbMesh = boundingBox.GetComponent<Renderer>();
+	        Bounds bbBounds = bbMesh.bounds;
+	        _minY = bbBounds.min.y;
+	        _maxY = bbBounds.max.y;
+	        _minX = bbBounds.min.x;
+	        _maxX = bbBounds.max.x;
+	    }
 	}
-	
-	// Update is called once per frame
+
+    // Update is called once per frame
 	void Update () {
 
         //transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * _playerSpeed * Time.deltaTime;
@@ -34,6 +57,6 @@ public class PlayerMovementController : MonoBehaviour {
             transform.Translate(_playerSpeed * Time.deltaTime, 0, 0);
         }
 
-        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -3.65f, 6.723f), Mathf.Clamp(transform.position.y,  -1.0f, 3.5f));
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, _minX+_spriteSizeX/2, _maxX-_spriteSizeX/2), Mathf.Clamp(transform.position.y,  _minY+_spriteSizeY/2, _maxY+_spriteSizeY/4), transform.position.z);
 	}
 }
