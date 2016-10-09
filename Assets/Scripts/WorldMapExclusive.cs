b4d8b6eae4ea712f8be32889788a01581f6ffb83
Chris Schaefbauer <schaefba@colorado.edu>
2016-10-09 15:53:51 -0700

    Add zone and world map visibility scripts.

---
+++ Assets/Scripts/WorldMapExclusive.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WorldMapExclusive : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (SceneManager.GetActiveScene().name == "WorldMap")
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
+	}
+}
---
+++ Assets/Scripts/ZoneMapExclusive.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ZoneMappable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (SceneManager.GetActiveScene().name.Contains("Zone"))
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
+	}
+}
